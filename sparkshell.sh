6964d248cac537cbe278298ec73feb4bd1e8a3f9
Ks228809 <168144585+Ks228809@users.noreply.github.com>
2024-04-30 13:15:14 -0400

    Update sparkshell.sh

--- sparkshell.sh
+++ sparkshell.sh
@@ -6,15 +6,18 @@ S3_BUCKET="s3://aws-logs-992382847407-us-east-1/EMAP/"
 # Define the local directory to store files
 LOCAL_DIR="/home/hadoop/EMAP_files"
 
+# Define the location of the JAR file and class name
+JAR_FILE="${LOCAL_DIR}/WineClassification.jar"
+MAIN_CLASS="wineClassification.LogisticRegressionPrediction"
+
 # Create local directory if it doesn't exist
 mkdir -p ${LOCAL_DIR}
 
 # Copy all files from the S3 bucket to the local directory
 aws s3 cp ${S3_BUCKET} ${LOCAL_DIR} --recursive
 
-# Install Python dependencies
-pip install -r ${LOCAL_DIR}/requirements.txt
+# Assuming the requirements include Java dependencies or setup
+# Here we could set Java environment or download necessary Java libraries
 
-# Submit the PySpark job
-spark-submit --master yarn ${LOCAL_DIR}/model_training.py
-
+# Run the Java training class
+java -cp ${JAR_FILE} ${MAIN_CLASS}
